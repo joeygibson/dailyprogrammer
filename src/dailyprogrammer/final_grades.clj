669f21f65077ff4ec7915955bae1aca421fd83d2
Joey Gibson <joey@joeygibson.com>
2014-06-19 23:18:00 -0400

    final_grades.clj seems to be working

--- src/dailyprogrammer/final_grades.clj
+++ src/dailyprogrammer/final_grades.clj
@@ -29,18 +29,65 @@
   [grades]
   (let [sum (reduce + grades)
         num (count grades)]
-    (double (/ sum num))))
+    (Math/round (double (/ sum num)))))
+
+(defn- between?
+  "Determines if the number is between two other numbers"
+  [num min max]
+  (and (>= num min)
+       (< num max)))
+
+(defn- compute-plus-or-minus
+  "Decides if there should be a + or - with the letter grade.
+  A + for the top 30 percentile, and a - for the bottom 30."
+  [score letter]
+  (let [perc (rem score 10)
+        plus-or-minus (cond
+                        (<= perc 3) "-"
+                        (and (>= perc 7)
+                             (not (= letter "A"))) "+"
+                        :else "")]
+    (format "%s%s" letter plus-or-minus)))
 
 (defn- compute-letter-grade
   "Compute the correct letter grade for the student's average score"
   [score]
-  "A")
+  (cond
+    (between? score 90 101) (compute-plus-or-minus score "A")
+    (between? score 80 90) (compute-plus-or-minus score "B")
+    (between? score 70 80) (compute-plus-or-minus score "C")
+    (between? score 60 70) (compute-plus-or-minus score "D")
+    :else "F"))
 
-(defn- format-grades
+(defn- process-student
   "Format the student naem and grade for display"
   [student-info]
   (let [[first-name last-name & grades] student-info
         average (compute-grade-average grades)
         letter-grade (compute-letter-grade average)
-        grades-str (apply (partial format "%d %d %d %d %d") grades)]
-    (format "%s %s (%f%%) (%s) %s" first-name last-name average letter-grade grades-str)))
+        sorted-grades (sort grades)]
+    [first-name last-name average letter-grade sorted-grades]))
+
+(defn- format-student
+  "Pretty-prints the data for each student"
+  [student-info]
+  (let [[first-name last-name average letter-grade grades] student-info
+        grades-str (apply (partial format "%3d %3d %3d %3d %3d") grades)]
+    (format "%-10s %-10s %3d%% %-2s %s" first-name last-name average letter-grade grades-str)))
+
+(defn process-grades
+  "Process all the grade records from the specified file"
+  [file-name]
+  (let [data (get-data-from-file file-name)
+        records (convert-scores-to-proper-type data)
+        processed-students (map process-student records)
+        sorted-students (sort-by #((juxt second first) (drop 1 %)) processed-students)]
+    (map format-student (reverse sorted-students))))
+
+(defn- print-grades
+  "Prints the grades after all the works is done"
+  [records]
+  (doseq [record records]
+    (println record)))
+
+(print-grades (process-grades "resources/final_grades.txt"))
